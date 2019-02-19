f1dd360021aeff26690c21dc1b40563c38aa5db0
Cryptobyte <cryptobyte1134@gmail.com>
2019-02-19 13:31:19 -0500

    FizzBuzz

---
+++ Challenges/FizzBuzz.playground/Contents.swift
@@ -0,0 +1,8 @@
+for i in 1...100 {
+    guard let message: String =
+        ((i % 3 == 0) && (i % 5 == 0)) ? "FizzBuzz" :
+            (i % 3 == 0) ? "Fizz" :
+                (i % 5 == 0) ? "Buzz" : nil else { continue }
+    
+    print(message)
+}

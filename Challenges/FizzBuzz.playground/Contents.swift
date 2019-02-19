00e92e734b6227712a6f9f2f12c8394e2b983d9d
Cryptobyte <cryptobyte1134@gmail.com>
2019-02-19 18:18:22 -0500

    Fix FizzBuzz

--- Challenges/FizzBuzz.playground/Contents.swift
+++ Challenges/FizzBuzz.playground/Contents.swift
@@ -2,7 +2,10 @@ for i in 1...100 {
     guard let message: String =
         ((i % 3 == 0) && (i % 5 == 0)) ? "FizzBuzz" :
             (i % 3 == 0) ? "Fizz" :
-                (i % 5 == 0) ? "Buzz" : nil else { continue }
+                (i % 5 == 0) ? "Buzz" : nil else {
+                    print(i)
+                    continue
+    }
     
     print(message)
 }
